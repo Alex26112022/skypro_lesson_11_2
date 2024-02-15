93edbaeab81f127fea08671e0cd54e7a28afba31
Алексей Денисенко <elvis.presly.039127@gmail.com>
2024-02-15 14:15:57 +0400

    add str_title() add docstring

--- str_func.py
+++ str_func.py
@@ -1,5 +1,13 @@
 def str_upper(input_str: str) -> str:
     """
     Принимает на вход строку и возвращает ее со всеми заглавными буквами.
+    Добавил docstring.
     """
     return input_str.upper()
+
+
+def str_title(input_str: str) -> str:
+    """
+    Делает заглавными первые буквы каждого слова в строке, поступившей на вход.
+    """
+    return input_str.title()
