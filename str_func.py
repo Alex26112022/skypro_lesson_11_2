8a871e933adeed2e8213d8b52ff11864cfb4ffde
Алексей Денисенко <elvis.presly.039127@gmail.com>
2024-02-15 14:46:01 +0400

    add if __name__ == '__main__'

--- str_func.py
+++ str_func.py
@@ -1,12 +1,17 @@
-def str_upper(input_str: str) -> str:
+def str_upper(input_str: str = '') -> str:
     """
     Принимает на вход строку и возвращает ее со всеми заглавными буквами.
     """
     return input_str.upper()
 
 
-def str_title(input_str: str) -> str:
+def str_title(input_str: str = '') -> str:
     """
     Делает заглавными первые буквы каждого слова в строке, поступившей на вход.
     """
     return input_str.title()
+
+
+if __name__ == '__main__':
+    str_upper()
+    str_title()
